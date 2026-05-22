773595b5330444c1557ca3c7b2369b0db686cde5
Maju Facirolli <mariajulia.facirolli@novahaus.com.br>
2026-05-22 11:26:07 -0300

    build: replaces classnames with clsx

--- apps/web/src/components/Tooltip.tsx
+++ apps/web/src/components/Tooltip.tsx
@@ -1,4 +1,4 @@
-import classNames from "classnames"
+import clsx from "clsx"
 
 interface ITooltipProps {
 	label: string
@@ -7,7 +7,7 @@ interface ITooltipProps {
 export const Tooltip = ({ label }: ITooltipProps) => {
 	return (
 		<span
-			className={classNames(
+			className={clsx(
 				"pointer-events-none absolute left-full ml-2 p-1.5 rounded-lg text-xs font-semibold text-red-800 bg-white",
 				"shadow-sm whitespace-nowrap opacity-0 invisible -translate-x-2",
 				"group-hover:opacity-100 group-hover:translate-x-0 group-hover:visible transition-all duration-200 ease-out",
