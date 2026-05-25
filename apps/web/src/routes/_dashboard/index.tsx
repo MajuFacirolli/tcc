9e35329d4ba047095262e061aaba4e7f2f21a4dd
Maju Facirolli <mariajulia.facirolli@novahaus.com.br>
2026-05-25 14:07:54 -0300

    feat: implements dashboard home page

--- apps/web/src/routes/_dashboard/index.tsx
+++ apps/web/src/routes/_dashboard/index.tsx
@@ -1,9 +1,31 @@
 import { createFileRoute } from "@tanstack/react-router"
+import { BloodBankSummary } from "@/components/BloodBankSummary"
+import { DailyMetrics } from "@/components/DailyMetrics"
+import { Heading } from "@/components/Heading"
+import { RecentCampaigns } from "@/components/RecentCampaigns"
 
 export const Route = createFileRoute("/_dashboard/")({
 	component: Index,
 })
 
 function Index() {
-	return <div>Dashboard</div>
+	return (
+		<div className="max-w-7xl mx-auto px-6 py-8 flex flex-col gap-10">
+			<Heading>
+				<Heading.Overline>Visão Geral</Heading.Overline>
+				<Heading.Title>Painel Administrativo</Heading.Title>
+				<Heading.Description>
+					Acompanhe em tempo real o estoque de sangue, os doadores elegíveis e o
+					desempenho das campanhas ativas.
+				</Heading.Description>
+			</Heading>
+
+			<DailyMetrics />
+
+			<div className="grid md:grid-cols-12 gap-y-10 gap-x-4">
+				<BloodBankSummary />
+				<RecentCampaigns />
+			</div>
+		</div>
+	)
 }
