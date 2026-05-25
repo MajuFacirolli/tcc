89dedc7065a78590cf20cae42e20f92cdef7b5c9
Maju Facirolli <mariajulia.facirolli@novahaus.com.br>
2026-05-25 09:04:08 -0300

    refactor: improves support section cards

--- apps/web/src/components/SupportSection.tsx
+++ apps/web/src/components/SupportSection.tsx
@@ -5,26 +5,30 @@ export const SupportSection = () => {
 	return (
 		<section className="flex flex-col gap-6">
 			<h3 className="typography-overline">Fale com o suporte</h3>
-			<div className="flex flex-col sm:flex-row items-center gap-4 max-w-3xl">
-				<Card>
-					<Card.Icon Icon={Mail} />
-					<div>
-						<Card.Title>E-mail</Card.Title>
-						<Card.Description className="text-xs">
-							Resposta em até 24h úteis
-						</Card.Description>
+			<div className="flex flex-col sm:flex-row items-center gap-4 max-w-xl">
+				<Card className="gap-2">
+					<div className="flex gap-2">
+						<Card.Icon Icon={Mail} />
+						<div>
+							<Card.Title>E-mail</Card.Title>
+							<Card.Description className="text-xs">
+								Resposta em até 24h úteis
+							</Card.Description>
+						</div>
 					</div>
 					<Card.Description className="text-red-800! font-semibold">
 						suporte@hemoconnect.com.br
 					</Card.Description>
 				</Card>
-				<Card>
-					<Card.Icon Icon={Phone} />
-					<div>
-						<Card.Title>Telefone</Card.Title>
-						<Card.Description className="text-xs">
-							Seg a Sex, das 8h às 18h
-						</Card.Description>
+				<Card className="gap-2">
+					<div className="flex gap-2">
+						<Card.Icon Icon={Phone} />
+						<div>
+							<Card.Title>Telefone</Card.Title>
+							<Card.Description className="text-xs">
+								Seg a Sex, das 8h às 18h
+							</Card.Description>
+						</div>
 					</div>
 					<Card.Description className="text-red-800! font-semibold">
 						(16) 3000-0000
