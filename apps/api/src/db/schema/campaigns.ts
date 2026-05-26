6d7a2b10e234e74872a7b868ba5d2be12dd5fd43
Maju Facirolli <mariajulia.facirolli@novahaus.com.br>
2026-05-26 16:05:52 -0300

    feat: creates campaign entity, table and related types

---
+++ apps/api/src/db/schema/campaigns.ts
@@ -0,0 +1,19 @@
+import { integer, pgTable, text, timestamp } from "drizzle-orm/pg-core"
+import { uuidv7 } from "uuidv7"
+import { bloodTypeEnum } from "./enums/bloodType"
+import { campaignStatusEnum } from "./enums/campaignStatus"
+
+export const campaigns = pgTable("campaigns", {
+	id: text()
+		.primaryKey()
+		.$defaultFn(() => uuidv7()),
+	title: text().notNull(),
+	message: text().notNull(),
+	bloodType: bloodTypeEnum().notNull(),
+	status: campaignStatusEnum().notNull().default("draft"),
+	totalEligibleDonors: integer().notNull().default(0),
+	notifiedCount: integer().notNull().default(0),
+	intentionConfirmationsCount: integer().notNull().default(0),
+	averageResponseTime: integer().notNull().default(0),
+	createdAt: timestamp().notNull().defaultNow(),
+})
