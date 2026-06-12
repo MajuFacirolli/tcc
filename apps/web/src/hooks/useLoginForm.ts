9a3b4a989e1407d99364cd82064879fd4e840501
Maju Facirolli <majufacirolli2004@hotmail.com>
2026-06-12 07:39:43 -0300

    chore: working on fake login

---
+++ apps/web/src/hooks/useLoginForm.ts
@@ -0,0 +1,24 @@
+import { PagesEnum } from "@/enums/PagesEnum"
+import { useRouter } from "@tanstack/react-router"
+import { useForm } from "react-hook-form"
+
+type LoginFields = {
+	email: string
+	password: string
+}
+
+export const useLoginForm = () => {
+	const { register, handleSubmit } = useForm<LoginFields>()
+	const router = useRouter()
+
+	function handleLoginSubmit(data: unknown) {
+		console.log(data)
+		localStorage.setItem("isAuthenticated", "true")
+		router.navigate({ to: PagesEnum.HOME })
+	}
+
+	return {
+		handleSubmit: handleSubmit(handleLoginSubmit),
+		register,
+	}
+}
--- apps/web/src/routes/_dashboard.tsx
+++ apps/web/src/routes/_dashboard.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute, Outlet } from "@tanstack/react-router"
+import { createFileRoute, Outlet, redirect } from "@tanstack/react-router"
 import { Sidebar } from "../components/Sidebar"
 
 const DashboardLayout = () => (
@@ -11,5 +11,10 @@ const DashboardLayout = () => (
 )
 
 export const Route = createFileRoute("/_dashboard")({
+	beforeLoad: () => {
+		if (!localStorage.getItem("isAuthenticated")) {
+			throw redirect({ to: "/login" })
+		}
+	},
 	component: DashboardLayout,
 })
