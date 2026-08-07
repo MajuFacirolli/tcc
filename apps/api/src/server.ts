8556d028c1074e70a7f6c480707edf77efe74707
Maju Facirolli <mariajulia.facirolli@novahaus.com.br>
2026-08-07 14:27:53 -0300

    build: configs api cors, cookie, jwt and rate limit

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,47 +1,7 @@
-import "reflect-metadata"
-import fastifyCors from "@fastify/cors"
-import fastifySwagger from "@fastify/swagger"
-import ScalarApiReference from "@scalar/fastify-api-reference"
-import { fastify } from "fastify"
-import {
-	jsonSchemaTransform,
-	serializerCompiler,
-	validatorCompiler,
-	type ZodTypeProvider,
-} from "fastify-type-provider-zod"
+import { buildApp } from "@/app"
 import { env } from "@/env"
-import { errorHandler } from "./presentation/middlewares/errorHandler"
-import { registerRoutes } from "./presentation/routes"
 
-const app = fastify().withTypeProvider<ZodTypeProvider>()
-
-app.setValidatorCompiler(validatorCompiler)
-app.setSerializerCompiler(serializerCompiler)
-
-app.register(fastifyCors, {
-	origin: true,
-	methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-	// credentials: true,
-})
-
-app.register(fastifySwagger, {
-	openapi: {
-		info: {
-			title: "HemoConnect API",
-			description:
-				"API for emergency and strategic mobilization of blood donors",
-			version: "1.0.0",
-		},
-	},
-	transform: jsonSchemaTransform,
-})
-
-app.register(ScalarApiReference, {
-	routePrefix: "/docs",
-})
-
-app.register(errorHandler)
-app.register(registerRoutes)
+const app = buildApp()
 
 app.listen({ port: env.PORT, host: "0.0.0.0" }).then(() => {
 	console.log(`🚀 HTTP server running on http://localhost:${env.PORT}`)
