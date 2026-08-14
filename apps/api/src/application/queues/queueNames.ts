407a25a4ee31b55ebdf1ca7e155f73372b0328ce
Maju Facirolli <mariajulia.facirolli@novahaus.com.br>
2026-08-14 14:26:25 -0300

    feat: implements close campaign job

--- apps/api/src/application/queues/queueNames.ts
+++ apps/api/src/application/queues/queueNames.ts
@@ -1,5 +1,6 @@
 export const QUEUE_NAMES = {
 	CAMPAIGN_EMAIL: "campaign-email",
+	CAMPAIGN_LIFECYCLE: "campaign-lifecycle",
 } as const
 
 export type QueueName = (typeof QUEUE_NAMES)[keyof typeof QUEUE_NAMES]
