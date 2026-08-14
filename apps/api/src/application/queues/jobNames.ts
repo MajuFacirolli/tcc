34a5bd167eb2b25b0da7864e34d83be19c0b632a
Maju Facirolli <mariajulia.facirolli@novahaus.com.br>
2026-08-14 14:26:25 -0300

    feat: implements close campaign job

--- apps/api/src/application/queues/jobNames.ts
+++ apps/api/src/application/queues/jobNames.ts
@@ -1,5 +1,6 @@
 export const JOB_NAMES = {
 	SEND_CAMPAIGN_EMAIL: "send-campaign-email",
+	CLOSE_CAMPAIGN: "close-campaign",
 } as const
 
 export type JobName = (typeof JOB_NAMES)[keyof typeof JOB_NAMES]
