983af7e1e23a424e948c1f8b0a1c25fa4dbe7dc0
Maju Facirolli <mariajulia.facirolli@novahaus.com.br>
2026-08-17 00:21:09 -0300

    feat: implements confirmation methods to generate token and create confirmation link

--- apps/api/src/application/interfaces/IConfirmationsRepository.ts
+++ apps/api/src/application/interfaces/IConfirmationsRepository.ts
@@ -1,5 +1,8 @@
 import type { ConfirmDonationIntentionOutput } from "../dtos/confirmations/ConfirmDonationIntentionOutput"
+import type { CreateConfirmationInput } from "../dtos/confirmations/CreateConfirmationInput"
 
 export interface IConfirmationsRepository {
+	generateToken(input: CreateConfirmationInput): Promise<string>
+	createConfirmationLink(token: string): string
 	confirm(token: string): Promise<ConfirmDonationIntentionOutput>
 }
